826c2bc40b63d2893e81afa7e8338e5187c3d675
erikrw <ering-walters@csumb.edu>
2016-05-24 14:10:27 -0700

    Tested code using unit testing first time ever :D

---
+++ CarsTester/Tester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Cars;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarsTester
+{
+    [TestClass]
+    public class Tester
+    {
+        [TestMethod]
+        public void ColorTest()
+        {
+            Car testcar = new Car();
+            testcar.Color = "blue";
+            Assert.IsTrue(testcar.Color == "blue");
+        }
+    }
+}
