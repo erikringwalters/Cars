3d0ca78b464d004a116233b8f1a370c71b19a7fa
erikrw <ering-walters@csumb.edu>
2016-05-23 19:08:07 -0700

    initial commit

---
+++ Cars/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        private string color;
+        public void set_color(string a)
+        {
+            color = a;
+        }
+        public string get_color()
+        {
+            return color;
+        }
+    }
+}
---
+++ CarsConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cars;
+
+namespace CarsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car carx = new Car();
+            carx.set_color("red");
+            Console.Out.WriteLine("The color is " + carx.get_color());
+            Console.In.ReadLine();
+        }
+    }
+}
