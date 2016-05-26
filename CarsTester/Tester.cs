380d132ab97981bd65e7fce876f515747f00d013
erikrw <ering-walters@csumb.edu>
2016-05-25 22:09:51 -0700

    Tested two cars

--- CarsTester/Tester.cs
+++ CarsTester/Tester.cs
@@ -17,9 +17,13 @@ namespace CarsTester
         public void EngineTest()
         {
             Car testcar = new Car();
+            Car testcar2 = new Car();
             testcar.Color = "blue";
+            testcar2.Color = "red";
             testcar.TurnCarOn();
             Assert.IsTrue(testcar.engineStatus() == true);
+            testcar2.TurnCarOn();
+            Assert.IsTrue(testcar2.engineStatus() == true);
             testcar.TurnCarOff();
             Assert.IsTrue(testcar.engineStatus() == false);
         }
